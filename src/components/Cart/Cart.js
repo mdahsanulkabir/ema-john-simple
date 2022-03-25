d18018e1e0ebabf1258747bf460b350680f586bc
ahsan <makabir98@yahoo.com>
2022-03-25 13:50:50 +0600

    local storage to cart link

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for(const product of cart){
+        quantity += product.quantity;
+        total += product.price * product.quantity;
+        shipping += product.shipping;
+    }
+    let tax = total * 0.1;
+    const grandTotal = (total + shipping + tax).toFixed(2);
+    tax = tax.toFixed(2);
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price : $ {total}</p>
+            <p>Total Shipping : $ {shipping}</p>
+            <p>Tax : $ {tax}</p>
+            <h5>Grand Total : {grandTotal}</h5>
         </div>
     );
 };
